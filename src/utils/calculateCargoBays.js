2d7f9c7235f49e83eeab0ccb73e67bb9bf947242
aleksandershurdho20m <aleksandershurdho@gmail.com>
2023-03-27 17:52:44 +0200

    fix: fixed variable naming

--- src/utils/calculateCargoBays.js
+++ src/utils/calculateCargoBays.js
@@ -5,10 +5,10 @@ export const calculateCargoBays = (cargobays) => {
     return accumulator + parseFloat(value);
   }, 0);
 
-  const data = cardoResults / 10;
-  const anohter = Number.isInteger(cardoResults) ? 0 : 1;
+  const calculatedCardoResults = cardoResults / 10;
+  const data = Number.isInteger(cardoResults) ? 0 : 1;
 
-  result = Math.floor(data) + anohter;
+  result = Math.floor(calculatedCardoResults) + data;
 
   return result;
 };
